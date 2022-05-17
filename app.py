e1187dee14997aa4284c357a20157722e151e594
sunnysavita10 <engsunny3365@gmail.com>
2022-05-17 16:35:03 +0530

    updated

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 def index():
     return "hello world!"
 
-
+@@app.route('/',methods=['GET'])
+def demo():
+    pass
 
 if __name__=='__main__':
     app.run(host="0.0.0.0",debug=True,port=os.environ['PORT'])
