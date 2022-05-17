2ada836a83ecce8385aba2800780d1d1dd2080ca
sunnysavita10 <engsunny3365@gmail.com>
2022-05-17 15:59:00 +0530

    done

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return "hello world!"
 
-app.run(debug=True)
+
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0",debug=True,port=os.environ['PORT'])
 
 
